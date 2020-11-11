84cb233076ac5a234f26049900141a7341fd26f1
Ayushi-Maheshwari15 <123ayushimaheshwari@gmail.com>
2020-11-11 22:09:46 +0530

    print the natural number by using php

---
+++ cal.php
@@ -0,0 +1,23 @@
+<html>
+    <body style="background-color:pink">
+        <div style="color:red">
+            <center style="font-size:20px">
+                <?php
+$num=$_POST['n'];
+$i=1;
+while($i<=$num){
+    echo$i;
+    echo"<br>";
+    $i++;
+}
+?>
+
+            </center>
+            
+        </div>
+        
+    </body>
+</html>
+
+
+
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>phpnatural</title>
+    </head>
+    <body style="background-color:pink">
+        <div style="color:blue">
+            <center>
+                <h1>print the number in a given range</h1>
+                <form method="POST" action="cal.php">
+                    <input type="number" name="n" placeholder="enter the range"><br>
+                    <input type="submit" value="perform">
+                </form>
+            </center>
+        </div>
+    </body>
+</html>
+
